6a4449a6523286f326a5d2f763cb7a14ce6a445c
gn410270 <dk4why@hotmail.com>
2015-03-11 19:40:29 +0800

    I learn to use math.h

--- hello world.cpp
+++ hello world.cpp
@@ -1,8 +1,10 @@
 #include<stdio.h>
+#include<math.h>
 int main()
 {
 	printf("hello world!!\n");
 	printf("%d\n",2+3);
 	printf("%f\n",1.2+34.234);
+	printf("%.16f\n",cos(2.0*M_PI/3.0)); //印到小數點16位 
 	return 0;
  } 
