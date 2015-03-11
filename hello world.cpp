13dd74e80b14fcab6f8e16ddbf280b12f675e6b6
gn410270 <dk4why@hotmail.com>
2015-03-11 20:36:28 +0800

    I print my name

--- hello world.cpp
+++ hello world.cpp
@@ -1,8 +1,19 @@
 #include<stdio.h>
 int main()
 {
+	char Name[50]="Shang-Heng Tsai";   
+	//Name is a fixed char vector
+	//Name[0],Name[1],Name[2],.......,Name[49]
+	//Name[0]=S
+	//Name[1]=h
+	//Name[2]=a
+	//Name[3]=n
+	//......
+	//Name[15]= \0
 	printf("hello world!!\n");
-	printf("%d\n",2+3);
-	printf("%f\n",1.2+34.234);
+	printf("%d\n",2+3);            // %d print an integer
+	printf("%f\n",1.2+34.234);     // %f print a float number
+	printf("%s\n",Name);
+	printf("%c\n",Name[5]);
 	return 0;
  } 
